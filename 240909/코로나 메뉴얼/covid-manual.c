ad21aa0bea8e696f133f5cf0eacc6b83d5c75eeb
chanwook01 <chanwook01@naver.com>
2024-09-09 21:17:55 +0900

    Title: 코로나 메뉴얼, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240909/코로나 메뉴얼/covid-manual.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main() {
+    int ter1, ter2, ter3;
+    char cou1, cou2, cou3;
+
+    scanf("%c %d\n", &cou1, &ter1);
+    scanf("%c %d\n", &cou2, &ter2);
+    scanf("%c %d", &cou3, &ter3);
+
+    if(cou1 == 'Y' && ter1 >= 37){
+        if(cou2 == 'Y' && ter2 >= 37){
+            printf("E");
+        }
+        else{
+            if(cou3 == 'Y' && ter3 >= 37){
+                printf("E");
+            }
+            else{
+                printf("N");
+            }
+        }
+    }
+    else if(cou2 == 'Y' && ter2 >= 37){
+        if(cou3 == 'Y' && ter3 >= 37){
+            printf("E");
+        }
+        else{
+            printf("N");
+        }
+    }
+    else{
+        printf("N");
+    }
+    return 0;
+}
