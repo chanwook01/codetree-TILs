d59d45f2cbdbd7ae28d8035b80362eb89831442c
chanwook01 <chanwook01@naver.com>
2024-09-10 10:59:01 +0900

    Title: 짝수만 출력 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240910/짝수만 출력 2/output-only-even-2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    
+    int a, b, i;
+
+    scanf("%d %d", &b, &a);
+
+    i = b;
+
+    while(i >= a){
+        printf("%d ", i);
+        i -= 2;
+    }
+    
+    return 0;
+}
