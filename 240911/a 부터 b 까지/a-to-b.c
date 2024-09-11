ec2993f53409092b0b64f20ae70414490a3a72e7
chanwook01 <chanwook01@naver.com>
2024-09-11 12:48:26 +0900

    Title: a 부터 b 까지, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240911/a 부터 b 까지/a-to-b.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    
+    int a, b;
+
+    scanf("%d %d", &a, &b);
+
+    while(a <= b){
+        printf("%d ", a);
+        if(a % 2 != 0){
+            a *= 2;
+        }
+        else{
+            a += 3;
+        }
+    }
+    return 0;
+}
