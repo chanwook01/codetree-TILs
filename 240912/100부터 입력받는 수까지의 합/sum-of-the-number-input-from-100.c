b273a2120e7afe1c78752943f67c70fe95f67984
chanwook01 <chanwook01@naver.com>
2024-09-12 12:40:13 +0900

    Title: 100부터 입력받는 수까지의 합, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240912/100부터 입력받는 수까지의 합/sum-of-the-number-input-from-100.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    
+    int n, sum = 0;
+
+    scanf("%d", &n);
+
+    for(int i = n; i <= 100; i++){
+        sum += i;
+    }
+
+    printf("%d", sum);
+    
+    return 0;
+}
