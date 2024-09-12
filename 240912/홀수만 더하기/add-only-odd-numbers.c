8b92036c6b282d834e55210863bff3f53d3e0175
chanwook01 <chanwook01@naver.com>
2024-09-12 12:55:36 +0900

    Title: 홀수만 더하기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240912/홀수만 더하기/add-only-odd-numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    
+    int n, a, sum = 0;
+
+    scanf("%d", &n);
+
+    for(int i = 1; i <= n; i++){
+        scanf("%d\n", &a);
+
+        if((a % 2 == 1) && (a % 3 == 0)){
+            sum += a;
+        }
+    }
+
+    printf("%d", sum);
+
+    return 0;
+}
