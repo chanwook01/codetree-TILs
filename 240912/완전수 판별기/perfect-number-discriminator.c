63bba7d73663a9a27020c4b46d71b031d9198444
chanwook01 <chanwook01@naver.com>
2024-09-12 18:11:08 +0900

    Title: 완전수 판별기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240912/완전수 판별기/perfect-number-discriminator.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    
+    int n;
+    int sum = 0;
+
+    scanf("%d", &n);
+
+    for(int i = 1; i <= n; i++){
+        if(n % i == 0){
+            sum += i;
+        }
+    }
+    sum -= n;
+    
+    if(n == sum){
+        printf("P");
+    }
+    else{
+        printf("N");
+    }
+
+    return 0;
+}
