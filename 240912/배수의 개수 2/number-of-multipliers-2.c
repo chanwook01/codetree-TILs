3dcc5b5aff7eaeca7d0e8dc9166d007c5b8319fb
chanwook01 <chanwook01@naver.com>
2024-09-12 12:21:35 +0900

    Title: 배수의 개수 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240912/배수의 개수 2/number-of-multipliers-2.c
+++ 240912/배수의 개수 2/number-of-multipliers-2.c
@@ -4,7 +4,7 @@ int main() {
     
     int a, cnt = 0;
 
-    for(int i = 1; i < 10; i++){
+    for(int i = 1; i <= 10; i++){
         scanf("%d\n", &a);
         if(a % 2 != 0){
             cnt ++;
