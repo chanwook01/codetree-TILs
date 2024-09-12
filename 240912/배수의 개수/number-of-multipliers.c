1cde733be789f49df476ba6fd69a2df6e599281d
chanwook01 <chanwook01@naver.com>
2024-09-12 12:24:36 +0900

    Title: 배수의 개수, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240912/배수의 개수/number-of-multipliers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    
+    int a, cnt3 = 0, cnt5 = 0;
+
+    for(int i = 1; i <= 10; i++){
+        scanf("%d\n", &a);
+
+        if(a % 3 == 0){
+            cnt3 ++;
+        }
+        if(a % 5 == 0){
+            cnt5 ++;
+        }
+    }
+
+    printf("%d %d", cnt3, cnt5);
+    
+    return 0;
+}
