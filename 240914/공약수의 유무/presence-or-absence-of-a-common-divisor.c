7c5ac80b17a69eb8dc1e9c8374bd750fa7cf5794
chanwook01 <chanwook01@naver.com>
2024-09-14 12:18:36 +0900

    Title: 공약수의 유무, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240914/공약수의 유무/presence-or-absence-of-a-common-divisor.c
+++ 240914/공약수의 유무/presence-or-absence-of-a-common-divisor.c
@@ -3,20 +3,20 @@
 int main() {
     
     int a, b;
-    bool x = false;
+    bool satisfied = false;
 
     scanf("%d %d", &a, &b);
     for(int i = a; i <= b; i++){
         if(1920 % i == 0 && 2880 % i == 0){
-            x = true;
+            satisfied = true;
         }
     }
-    if(x == true){
+    if(satisfied == true){
         printf("1");
     }
     else{
         printf("0");
     }
-    
+
     return 0;
 }
