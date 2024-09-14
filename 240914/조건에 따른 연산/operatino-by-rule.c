94457d64735a6d4845ba141b1bfccffd9eb5b8a1
chanwook01 <chanwook01@naver.com>
2024-09-14 11:58:33 +0900

    Title: 조건에 따른 연산, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240914/조건에 따른 연산/operatino-by-rule.c
+++ 240914/조건에 따른 연산/operatino-by-rule.c
@@ -8,21 +8,19 @@ int main() {
     scanf("%d", &n);
 
     while(1){
-        
-        if(n % 2 == 0){
-            n = n * 3 + 1;
-            cnt ++;
-        }
-        else{
-            n = n * 2 + 1;
-            cnt ++;
-        }
-
         if(n >= 1000){
-            printf("%d", cnt);
             break;
         }
+        if(n % 2 == 0){
+            n = n * 3 + 1;
+        }
+        else{
+            n = n * 2 + 2;
+        }
+        cnt++;
     }
 
+    printf("%d", cnt);
+    
     return 0;
 }
