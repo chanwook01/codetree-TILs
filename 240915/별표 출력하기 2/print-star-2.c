ea20cfd63b222e8ce062457ee9a221ac51d170e4
chanwook01 <chanwook01@naver.com>
2024-09-15 21:37:57 +0900

    Title: 별표 출력하기 2, Time: 9ms, Memory: 0MB, Status: Passed - CodeTree

--- 240915/별표 출력하기 2/print-star-2.c
+++ 240915/별표 출력하기 2/print-star-2.c
@@ -6,8 +6,8 @@ int main() {
 
     scanf("%d", &n);
 
-    for(int i = n; i >= 1; i--){
-        for(int j = 1; j <= i; j++)
+    for(int i = 1; i <= n; i++){
+        for(int j = 1; j <= n - i + 1; j++)
             printf("* ");
         printf("\n");
     }
