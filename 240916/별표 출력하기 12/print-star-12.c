8dc63ef4f95df3a15a4bc7974000dc53410c3b3f
chanwook01 <chanwook01@naver.com>
2024-09-16 17:08:05 +0900

    Title: 별표 출력하기 12, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240916/별표 출력하기 12/print-star-12.c
+++ 240916/별표 출력하기 12/print-star-12.c
@@ -6,7 +6,7 @@ int main() {
 
     for(int i = 0; i < n; i++){
         for(int j = 0; j < n; j++){
-            if(i == 0 || (j % 2 == 1 && j <= i)){
+            if(i == 0 || (j % 2 == 1 && i <= j)){
                 printf("* ");
             }
             else{
