23fda3f58fd77eb466088cdaf0f2fa300d52a630
chanwook01 <chanwook01@naver.com>
2024-09-18 17:22:12 +0900

    Title: 모양대로 숫자 출력하기, Time: 5ms, Memory: 0MB, Status: Passed - CodeTree

--- 240918/모양대로 숫자 출력하기/print-out-numbers-in-specific-shape.c
+++ 240918/모양대로 숫자 출력하기/print-out-numbers-in-specific-shape.c
@@ -3,19 +3,15 @@
 int main() {
     int n;
     scanf("%d", &n);
-    int cnt = 0;
-    for(int i = 0; i < n; i++){
-        for(int j = 1; j < i + 1; j++){
-            printf("  ");
-            
+    for(int i = n; i >= 1; i--){
+        for(int j = n; j >= 1; j--){
+            if(j > i)
+                printf("  ");
+            else
+                printf("%d ", j);
         }
-        for(int j = 0; j < n; j++){
-            if(n - j - cnt > 0){
-                printf("%d ", n - j - cnt);
-            }
-        }
-        cnt ++;
         printf("\n");
+    
     }
     return 0;
 }
