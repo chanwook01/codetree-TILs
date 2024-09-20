01ffac54ca295f1eb0e408a1be090fab62a01d8b
chanwook01 <chanwook01@naver.com>
2024-09-20 23:37:29 +0900

    Title: 특정 숫자 도달하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240920/특정 숫자 도달하기/reaching-specific-number.c
+++ 240920/특정 숫자 도달하기/reaching-specific-number.c
@@ -4,7 +4,7 @@ int main() {
     int arr[10];
     int total_sum = 0;
     int cnt = 0;
-    for(int i = 1; i <= 10; i ++){
+    for(int i = 1; i < 10; i ++){
         scanf("%d ", &arr[i]);
         if(arr[i] >= 250){
             break;
@@ -14,7 +14,7 @@ int main() {
             total_sum += arr[i];
         }
     }
-    printf("%d %.1lf", total_sum, (double)(total_sum) / cnt);
+    printf("%d %.1lf", total_sum, (double)total_sum / cnt);
 
     return 0;
 }
