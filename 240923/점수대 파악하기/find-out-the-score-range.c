493e23914220f27c6fd208c4bd226b5d2c68b221
chanwook01 <chanwook01@naver.com>
2024-09-23 20:26:53 +0900

    Title: 점수대 파악하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240923/점수대 파악하기/find-out-the-score-range.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int num[11] = {0,};
+    int arr[100];
+
+    for(int i = 0; i < 100; i++){
+        scanf("%d ", &arr[i]);
+        num[arr[i]/10]++;
+        if(arr[i] == 0)
+            break;
+    }
+
+    for(int i = 10; i >= 1; i--){
+        printf("%d - %d\n", i * 10, num[i]);
+    }
+    return 0;
+}
