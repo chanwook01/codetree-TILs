de6665ae72c390805a55e09e2328446540f88db0
chanwook01 <chanwook01@naver.com>
2024-09-23 20:01:19 +0900

    Title: 전항의 두 배, Time: 9ms, Memory: 0MB, Status: Passed - CodeTree

--- 240923/전항의 두 배/twice-the-previous.c
+++ 240923/전항의 두 배/twice-the-previous.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
 
 int main() {
-    int arr[10];
     int a, b;
     scanf("%d %d", &a, &b);
-    arr[0] = a;
-    arr[1] = b;
+    int arr[10] = {a, b};
 
     for(int i = 2; i < 10; i++){
         arr[i] = arr[i - 1] + 2 * arr[i - 2];
