4f9bf2983d51afe62489c51a745d79f72358e0d1
chanwook01 <chanwook01@naver.com>
2024-09-24 12:08:03 +0900

    Title: 배열 놀이, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240924/배열 놀이/play-with-array.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+int main() {
+    int n, q;
+    int a;
+    int b, c;
+    scanf("%d %d\n", &n, &q);
+    int arr[100];
+    for(int i = 0; i < n; i++){
+        scanf("%d ", &arr[i]);
+    }
+    for(int i = 0; i < q; i++){
+        scanf("%d ", &a);
+        if(a == 1){
+            scanf("%d", &b);
+            printf("%d", arr[b-1]);
+    }
+        else if(a == 2){
+            scanf("%d", &b);
+            for(int i = 0; i < n; i++){
+                if(arr[i] == b){
+                    printf("%d", i + 1);
+                    break;
+                }
+                if(arr[n-1] != b){
+                    printf("0");
+                }
+            
+            }
+        }
+
+        else{
+            scanf("%d %d", &b, &c);
+            for(int i = b - 1; i <= c - 1; i++){
+                printf("%d ", arr[i]);
+            }
+
+        }
+        printf("\n");
+    }
+    
+    return 0;
+}
