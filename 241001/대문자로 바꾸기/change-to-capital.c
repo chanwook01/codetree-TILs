a73820fd9164626d961bb4528595d74e46d0991b
chanwook01 <chanwook01@naver.com>
2024-10-01 19:24:46 +0900

    Title: 대문자로 바꾸기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241001/대문자로 바꾸기/change-to-capital.c
+++ 241001/대문자로 바꾸기/change-to-capital.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main() {
-    int arr[5][3];
+    char arr[5][3];
     for(int i = 0; i < 5; i++){
         for(int j = 0; j < 3; j++){
             scanf("%c ", &arr[i][j]);
